a1ec9ccecde3ed68fb7b85154f128557aef0bc98
Catherine Straus <vnair611@gmail.com>
2020-06-22 06:13:44 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -1,14 +1,16 @@
 var kittens = ["Milo", "Otis", "Garfield"]
 var kittens2 = ["Cat", "Cattius", "Catastrophic"]
 
-function combineArrays(array1, array2){
-  kitten3 = array.concat(array2)
-  return kitten3
+
+function functiondestructivelyAppendKitten(array){
+  
 }
-
-
-destructivelyAppendKitten
 
 function destructivelyRemoveLastKitten(array){
   array.slice(0,2)
+}
+
+function combineArrays(array1, array2){
+  kitten3 = array.concat(array2)
+  return kitten3
 }
