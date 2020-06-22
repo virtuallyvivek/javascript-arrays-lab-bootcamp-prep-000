dee232d5f6a81217800c4b443c03b3c54d2be491
Catherine Straus <vnair611@gmail.com>
2020-06-22 06:19:35 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -4,20 +4,25 @@ var kittens2 = ["Cat", "Cattius", "Catastrophic"]
 
 function functiondestructivelyAppendKitten(name){
   kittens.push(name)
+  return kittens
 }
 
 function destructivelyPrependKitten(name){
   kittens.unshift(name)
+  return kittens
 }
 
 function destructivelyRemoveLastKitten(array){
   kittens.pop()
+  return kittens
 }
 
 function destructivelyRemoveFirstKitten(){
   kittens.shift()
+  return kittens
 }
 
+function appendKitten()
 
 function combineArrays(array1, array2){
   kitten3 = array.concat(array2)
