1dad4d6f7de0345be072f6b90cb988bd7da44442
Catherine Straus <vnair611@gmail.com>
2020-06-22 06:09:28 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -1,12 +1,11 @@
 var kittens = ["Milo", "Otis", "Garfield"]
 var kittens2 = ["Cat", "Cattius", "Catastrophic"]
 
-functions removeBeginning(array1, array2){
+functions combineArrays(array1, array2){
   kitten3 = array.concat(array2)
   return kitten3
 }
 
-
 functions removeEnding(array){
-  array.slice(1)
+  array.slice(3)
 }
